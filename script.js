47de2198b11c7975c9a97bbe126cfef50621b4db
Johnny132 <jdkakareka@hotmail.com>
2024-09-19 20:05:50 -0400

    fixed bug

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function downloadResume() {
     const link = document.createElement('a');
-    link.href = 'reseme.pdfw'; // Ensure this path is correct
+    link.href = 'reseme.pdf'; // Ensure this path is correct
     link.download = 'JohnKakareka_Resume.pdf';
     document.body.appendChild(link);
     link.click();

53772f9a49fbf221227716afb51bd13a38818233
Johnny132 <jdkakareka@hotmail.com>
2024-11-11 08:29:10 -0500

    Popup Update

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function downloadResume() {
     const link = document.createElement('a');
-    link.href = 'reseme.pdf'; // Ensure this path is correct
+    link.href = 'Resume.pdf'; // Ensure this path is correct
     link.download = 'JohnKakareka_Resume.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
 }
+window.onload = function() { 
+    const popups = document.querySelectorAll('.popup'); 
+    popups.forEach(popup => { 
+        popup.style.display = 'flex'; 
+    }); 
+};
+function closePopup(popupId) { 
+    document.getElementById(popupId).style.display = 'none';}
